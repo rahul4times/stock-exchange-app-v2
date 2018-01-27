448a13fd9be3498eb9e9202b4a47c2baa923cf31
rahul4times <tkdmaster.sharma@gmail.com>
2018-01-27 08:29:58 -0700

    stock financials compeleted

--- app/assets/javascripts/channels/user_stock_balancesheet.js
+++ app/assets/javascripts/channels/user_stock_balancesheet.js
@@ -2,7 +2,7 @@ App.cable.subscriptions.create('StockChannel', {
   received: function(data) {
     let responseFromChannel = data;
 
-    //console.log("Financials: ", responseFromChannel.users_stock.financials.financials);
+    console.log("Financials: ", responseFromChannel.users_stock);
 
     this.renderBalanceSheet(responseFromChannel);
     return;
@@ -29,32 +29,103 @@ App.cable.subscriptions.create('StockChannel', {
 
       for(let i=0; i<data.length; i++){
 
-        if(data[i].currentAssets < 0){
-          currAssets.children[i+1].innerHTML =  "<span class='text-danger'>" + (data[i].currentAssets / 1000).toLocaleString() + "</span>";
-        } else {
-          currAssets.children[i+1].innerHTML =  "<span class='text-success'>" + (data[i].currentAssets / 1000).toLocaleString() + "</span>";
+        if(data[i].currentAssets > 0){
+          currAssets.children[i+1].className =  "text-right text-success";
+        } else if(data[i].currentAssets < 0){
+          currAssets.children[i+1].className =  "text-right text-danger";
+        } else{
+          currAssets.children[i+1].className =  "text-right";
         }
-
         currAssets.children[i+1].innerHTML = (data[i].currentAssets / 1000).toLocaleString();
 
+        if(data[i].totalAssets > 0){
+          totalAssets.children[i+1].className =  "text-right text-success";
+        } else if(data[i].totalAssets < 0){
+          totalAssets.children[i+1].className =  "text-right text-danger";
+        } else{
+          totalAssets.children[i+1].className =  "text-right";
+        }
         totalAssets.children[i+1].innerHTML = (data[i].totalAssets / 1000).toLocaleString();
 
+        if(data[i].totalLiabilities > 0){
+          totalLiablity.children[i+1].className =  "text-right text-success";
+        } else if(data[i].totalLiabilities < 0){
+          totalLiablity.children[i+1].className =  "text-right text-danger";
+        } else{
+          totalLiablity.children[i+1].className =  "text-right";
+        }
         totalLiablity.children[i+1].innerHTML = (data[i].totalLiabilities / 1000).toLocaleString();
 
+        if(data[i].currentCash > 0){
+          currCash.children[i+1].className =  "text-right text-success";
+        } else if(data[i].currentCash < 0){
+          currCash.children[i+1].className =  "text-right text-danger";
+        } else{
+          currCash.children[i+1].className =  "text-right";
+        }
         currCash.children[i+1].innerHTML = (data[i].currentCash / 1000).toLocaleString();
 
+        if(data[i].currentDebt > 0){
+          currDebt.children[i+1].className =  "text-right text-success";
+        } else if(data[i].currentDebt < 0){
+          currDebt.children[i+1].className =  "text-right text-danger";
+        } else {
+          currDebt.children[i+1].className =  "text-right";
+        }
         currDebt.children[i+1].innerHTML = (data[i].currentDebt / 1000).toLocaleString();
 
+        if(data[i].totalCash > 0){
+          totCash.children[i+1].className =  "text-right text-success";
+        } else if(data[i].totalCash < 0){
+          totCash.children[i+1].className =  "text-right text-danger";
+        } else {
+          totCash.children[i+1].className =  "text-right";
+        }
         totCash.children[i+1].innerHTML = (data[i].totalCash / 1000).toLocaleString();
 
+        if(data[i].totalDebt > 0){
+          totDebt.children[i+1].className =  "text-right text-success";
+        } else if(data[i].totalDebt < 0){
+          totDebt.children[i+1].className =  "text-right text-danger";
+        } else {
+          totDebt.children[i+1].className =  "text-right";
+        }
         totDebt.children[i+1].innerHTML = (data[i].totalDebt / 1000).toLocaleString();
 
+        if(data[i].shareholderEquity > 0){
+          shareEq.children[i+1].className =  "text-right text-success";
+        } else if(data[i].shareholderEquity < 0){
+          shareEq.children[i+1].className =  "text-right text-danger";
+        } else {
+          shareEq.children[i+1].className =  "text-right";
+        }
         shareEq.children[i+1].innerHTML = (data[i].shareholderEquity / 1000).toLocaleString();
 
+        if(data[i].cashChange > 0){
+          cashChange.children[i+1].className =  "text-right text-success";
+        } else if(data[i].cashChange < 0){
+          cashChange.children[i+1].className =  "text-right text-danger";
+        } else {
+          cashChange.children[i+1].className =  "text-right";
+        }
         cashChange.children[i+1].innerHTML = (data[i].cashChange / 1000).toLocaleString();
 
+        if(data[i].cashFlow > 0){
+          cashFlow.children[i+1].className =  "text-right text-success";
+        } else if(data[i].cashFlow < 0){
+          cashFlow.children[i+1].className =  "text-right text-danger";
+        } else {
+          cashFlow.children[i+1].className =  "text-right";
+        }
         cashFlow.children[i+1].innerHTML = (data[i].cashFlow / 1000).toLocaleString();
 
+        if(data[i].operatingGainsLosses > 0){
+          opGainLoss.children[i+1].className =  "text-right text-success";
+        } else if(data[i].operatingGainsLosses < 0){
+          opGainLoss.children[i+1].className =  "text-right text-danger";
+        } else {
+          opGainLoss.children[i+1].className =  "text-right";
+        }
         opGainLoss.children[i+1].innerHTML = (data[i].operatingGainsLosses / 1000).toLocaleString();
 
       }
--- app/assets/javascripts/channels/user_stock_financials.js
+++ app/assets/javascripts/channels/user_stock_financials.js
@@ -25,15 +25,73 @@ App.cable.subscriptions.create('StockChannel', {
     let costRev = document.getElementById('cost_rev');
     let opRev = document.getElementById('op_rev');
     let opInc = document.getElementById('op_in');
+    let netIncome = document.getElementById('net_in');
     let resDev = document.getElementById('res_dev');
     let opExp = document.getElementById('op_ex');
 
     for(let i=0; i<data.length; i++){
+
+      if(data[i].grossProfit > 0){
+        gProfit.children[i+1].className =  "text-right text-success";
+      } else if(data[i].grossProfit < 0){
+        gProfit.children[i+1].className =  "text-right text-danger";
+      } else {
+        gProfit.children[i+1].className =  "text-right";
+      }
       gProfit.children[i+1].innerHTML = (data[i].grossProfit / 1000).toLocaleString();
+
+      if(data[i].costOfRevenue > 0){
+        costRev.children[i+1].className =  "text-right text-success";
+      } else if(data[i].costOfRevenue < 0){
+        costRev.children[i+1].className =  "text-right text-danger";
+      } else {
+        costRev.children[i+1].className =  "text-right";
+      }
       costRev.children[i+1].innerHTML = (data[i].costOfRevenue / 1000).toLocaleString();
+
+      if(data[i].operatingRevenue > 0){
+        opRev.children[i+1].className =  "text-right text-success";
+      } else if(data[i].operatingRevenue < 0){
+        opRev.children[i+1].className =  "text-right text-danger";
+      } else {
+        opRev.children[i+1].className =  "text-right";
+      }
       opRev.children[i+1].innerHTML = (data[i].operatingRevenue / 1000).toLocaleString();
+
+      if(data[i].operatingIncome > 0){
+        opInc.children[i+1].className =  "text-right text-success";
+      } else if(data[i].operatingIncome < 0){
+        opInc.children[i+1].className =  "text-right text-danger";
+      } else {
+        opInc.children[i+1].className =  "text-right";
+      }
       opInc.children[i+1].innerHTML = (data[i].operatingIncome / 1000).toLocaleString();
+
+      if(data[i].netIncome > 0){
+        netIncome.children[i+1].className =  "text-right text-success";
+      } else if (data[i].netIncome < 0){
+        netIncome.children[i+1].className =  "text-right text-danger";
+      } else{
+        netIncome.children[i+1].className =  "text-right";
+      }
+      netIncome.children[i+1].innerHTML = (data[i].netIncome / 1000).toLocaleString();
+
+      if(data[i].researchAndDevelopment > 0){
+        resDev.children[i+1].className =  "text-right text-success";
+      } else if(data[i].researchAndDevelopment < 0){
+        resDev.children[i+1].className =  "text-right text-danger";
+      } else {
+        resDev.children[i+1].className =  "text-right";
+      }
       resDev.children[i+1].innerHTML = (data[i].researchAndDevelopment / 1000).toLocaleString();
+
+      if(data[i].operatingExpense > 0){
+        opExp.children[i+1].className =  "text-right text-success";
+      } else if(data[i].operatingExpense < 0){
+        opExp.children[i+1].className =  "text-right text-danger";
+      } else {
+        opExp.children[i+1].className =  "text-right";
+      }
       opExp.children[i+1].innerHTML = (data[i].operatingExpense / 1000).toLocaleString();
 
     }
