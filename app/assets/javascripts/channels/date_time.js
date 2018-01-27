f3bb01308945905463dc38097def65ee1a263a5d
rahul4times <tkdmaster.sharma@gmail.com>
2018-01-27 16:07:40 -0700

    login, signup and recover password style done

--- app/assets/javascripts/channels/date_time.js
+++ app/assets/javascripts/channels/date_time.js
@@ -7,14 +7,14 @@ function convertToServerTimeZone() {
   utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000);
   serverDate = new Date(utc + (3600000 * offset));
 
-  document.getElementById("market").innerHTML = serverDate.toLocaleString();
+  document.getElementById("market").innerHTML = serverDate.toDateString() + " | " + serverDate.toLocaleTimeString();
 
   if ((clientDate.getHours() > 6 && clientDate.getHours() < 13) &&
         (clientDate.getDay() > 1 && clientDate.getDay() < 6)) {
-    document.getElementById('date_time').innerHTML = 'Market is open ' + ' <span class="text-success"><i class="fa fa-circle" aria-hidden="true"></i></span>';
+    document.getElementById('date_time').innerHTML = 'Market is open ' + ' <span class="text-success"><small><i class="fa fa-circle" aria-hidden="true"></i></small></span>';
   }
   else {
-    document.getElementById('date_time').innerHTML = 'Market is close ' + ' <span class="text-danger"><i class="fa fa-circle" aria-hidden="true"></i></span>';
+    document.getElementById('date_time').innerHTML = 'Market is closed ' + ' <span class="text-danger"><small><i class="fa fa-circle" aria-hidden="true"></i></small></span>';
   }
 }
 
